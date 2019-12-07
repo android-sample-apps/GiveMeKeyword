cb977733d0ddd1ad5fc7eeedfba798a1afe6c017
jaeryo2357 <jaeryo2357@naver.com>
2019-12-07 23:51:07 +0900

    Firebase 추가

---
+++ app/src/main/java/com/mut_jaeryo/givmkeyword/OpenSourceActivity.kt
@@ -0,0 +1,12 @@
+package com.mut_jaeryo.givmkeyword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OpenSourceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_source)
+    }
+}
